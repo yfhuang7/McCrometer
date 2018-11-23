7637ba0be13f2b2f6f439030f2faccdc4c4afaeb
Yu-Fen Huang <yfhuang@hawaii.edu>
2018-11-23 11:50:31 -1000

    revised upload_McCrometer.sh

--- upload_McCrometer.sh
+++ upload_McCrometer.sh
@@ -2,15 +2,19 @@
 
 ### Updating ===============================
 echo updating index.html
-cd /home/yfhuang/Documents
-R -e "rmarkdown::render('McCrometer_Visualization.Rmd',output_file='index.html')"
+cd /home/yfhuang/Documents   # The directory where you put your scripts
+
+Rscript McCrometer_Download.R   # Run the R script for downloading data
+
+# Run the Rmarkdown for excute the .html that you want to put on your website
+R -e "rmarkdown::render('McCrometer_Visualization.Rmd',output_file='index.html')"   
 
 
 ### Uploading via git ======================
 echo uploading the website
 
-cd /home/yfhuang/Documents/github/TsangStreamLab.github.io
-cp ../../index.html .
+cd /home/yfhuang/Documents/github/TsangStreamLab.github.io  # the directory for your gitpage directory
+cp ../../index.html .  # copy the .html file to your git directory
 git add index.html
 git commit -m "uploaded index.html"
 git push
